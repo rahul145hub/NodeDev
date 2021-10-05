d6dacabe3446f99cb5c4d081a641cd207a2aba5f
Rahul <bhatiarahul145@gmail.com>
2021-10-05 21:46:58 +0530

    non strict this

---
+++ nonStrict.js
@@ -0,0 +1,31 @@
+// this keyword in nodejs non-strict 
+
+// 1.globally
+console.log(this);    // return empty obj {}
+
+
+function help() {
+    // inside function
+    console.log(this); // return global obj
+}
+
+help();
+
+let obj = {
+    name: "Ram",
+    fun: function () {
+        console.log(this);
+
+        function insideFun() {
+            console.log(this);
+        }
+
+        // 4.Obj fun fun
+        insideFun(); // return global obj
+    }
+}
+
+// 3.Obj fun
+obj.fun();   // return obj
+
+
