c8776a3c1eac1e249a703661275fe6e6736b2f4f
Rahul <bhatiarahul145@gmail.com>
2021-10-10 16:23:05 +0530

    DOne

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const app = express()
 const fs = require('fs');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello</h1>")
+    const filePath = path.join(__dirname, './views/index.html');
+    res.status(404).sendFile(filePath);
 })
 
-app.get('*', (req, res) => {
-    fs.readFile('page404.html', 'utf-8', (err, data) => {
-        if (!err) {
-            res.end(data)
-        }
-    })
+app.get('/about', (req, res) => {
+    const filePath = path.join(__dirname, './views/about.html');
+    res.status(404).sendFile(filePath);
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+app.use((req, res) => {
+    const filePath = path.join(__dirname, './views/page404.html');
+    res.status(404).sendFile(filePath);
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
-})
+});
